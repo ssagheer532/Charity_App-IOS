20e03255182619c520ebdb0527d4e675829614e3
Ahad <ssagheer@purdue.edu>
2017-01-06 18:05:15 -0500

    refined the buttons, which will become bubbles, floating around and made them bounce off eachother smoothly and naturally

--- Charity App/ViewController.swift
+++ Charity App/ViewController.swift
@@ -9,61 +9,97 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    //var movementX = 0.1
-    //var movementY = 0.1
-    var cordinate2dArray: [[Double]] = [ [0.1, 0.1], [0.1, 0.1]]
     
+    var cordinate2dArray: [[Double]] = [ [0.1, 0.1], [0.1, 0.1], [0.1, 0.1]]
     
+    var numberOfButtons = 10
     @IBAction func moveButton(_ button: UIButton) {
-   
         var movementX = 0.1
         var movementY = 0.1
         
-        if (button.tag == 2){
-            movementX = cordinate2dArray[1][0]
-            movementY = cordinate2dArray[1][1]
-        }
+        movementX = cordinate2dArray[button.tag-1][0]
+        movementY = cordinate2dArray[button.tag-1][1]
         
-        
-        if (button.tag == 1){
-            movementX = cordinate2dArray[0][0]
-            movementY = cordinate2dArray[0][1]
-        }
         
         if button.center.x >= view.frame.maxX {
             movementX = -movementX
         }
-        if button.center.x <= view.frame.minX {
+        if button.center.x <= view.frame.minX+1 {
             movementX = -movementX
         }
         if button.center.y >= view.frame.maxY {
             movementY = -movementY
         }
-        if button.center.y <= view.frame.minY {
+        if button.center.y <= view.frame.minY+1 {
             movementY = -movementY
         }
         
         
         button.center.x = button.center.x + CGFloat(movementX)
         button.center.y = button.center.y + CGFloat(movementY)
+        
+        cordinate2dArray[button.tag-1][0] = movementX
+        cordinate2dArray[button.tag-1][1] = movementY
+        bounceOff(button: button)
+        
+    }
+    
+    func bounceOff(button: UIButton){
+        
+        let buttonX1 = button.center.x + (button.frame.width/2)
+        let buttonX2 = button.center.x - (button.frame.width/2)
+        let buttonY1 = button.center.y + (button.frame.height/2)
+        let buttonY2 = button.center.y - (button.frame.height/2)
+        
+        for view in self.view.subviews as [UIView] {
+            
+            
+            if let btn = view as? UIButton{
+                
+                
+                let buttonCurrentX1 = btn.center.x + (btn.frame.width/2)
+                let buttonCurrentX2 = btn.center.x - (btn.frame.width/2)
+                let buttonCurrentY1 = btn.center.y + (btn.frame.height/2)
+                let buttonCurrentY2 = btn.center.y - (btn.frame.height/2)
+                
+                if (buttonCurrentX2 > buttonX2 && buttonCurrentX2 < buttonX1) ||
+                   (buttonCurrentX1 < buttonX1 && buttonCurrentX1 > buttonX2){
+                    
+                    if (buttonCurrentY1 < buttonY1 && buttonCurrentY1 > buttonY2) ||
+                       (buttonCurrentY1 > buttonY1 && buttonCurrentY1 < buttonY2){
+                        
+                        if cordinate2dArray[button.tag-1][1] == cordinate2dArray[btn.tag-1][1] {
+                            cordinate2dArray[button.tag-1][0] =  -cordinate2dArray[button.tag-1][0]
+                            cordinate2dArray[btn.tag-1][0] =  -cordinate2dArray[btn.tag-1][0]
 
-        if (button.tag == 1){
-            cordinate2dArray[0][0] = movementX
-            cordinate2dArray[0][1] = movementY
-        }
-        if (button.tag == 2){
-            cordinate2dArray[1][0] = movementX
-            cordinate2dArray[1][1] = movementY
+                        }
+                            
+                        else if cordinate2dArray[button.tag-1][0] == cordinate2dArray[btn.tag-1][0] {
+                            cordinate2dArray[button.tag-1][1] =  -cordinate2dArray[button.tag-1][1]
+                            cordinate2dArray[btn.tag-1][1] =  -cordinate2dArray[btn.tag-1][1]
+                            
+                        }
+                            
+                        else{
+
+                        cordinate2dArray[button.tag-1][0] =  -cordinate2dArray[button.tag-1][0]
+                        cordinate2dArray[button.tag-1][1] =  -cordinate2dArray[button.tag-1][1]
+                        cordinate2dArray[btn.tag-1][0] =  -cordinate2dArray[btn.tag-1][0]
+                        cordinate2dArray[btn.tag-1][1] =  -cordinate2dArray[btn.tag-1][1]
+                        }
+                        
+                    }
+                }
+
+            }
         }
     }
     
     
     @IBOutlet weak var temp: UILabel!
-    
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
-    
+    @IBOutlet weak var button3: UIButton!
     
     var timer = Timer()
     
@@ -78,19 +114,15 @@ class ViewController: UIViewController {
         temp.text! = String(counter)
         counter += 1
         moveButton(button2)
-       moveButton(button1)
-
+        moveButton(button1)
+        moveButton(button3)
+        
     }
-    
-    
     
     override func viewDidLoad() {
         button1.tag = 1
         button2.tag = 2
-        
-     
-        
-        
+        button3.tag = 3
         
         scheduledTimerWithTimeInterval()
         
