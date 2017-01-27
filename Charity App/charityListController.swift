87ed42deb7a135a54ade0bda4fe514970cfc1781
Ahad <ssagheer@purdue.edu>
2017-01-27 18:57:14 -0500

    tried adding a table view to display text nicely but so far it is not working

---
+++ Charity App/charityListController.swift
@@ -0,0 +1,10 @@
+//
+//  charityListController.swift
+//  Charity App
+//
+//  Created by Ahad Sagheer on 1/27/17.
+//  Copyright © 2017 Ahad Sagheer. All rights reserved.
+//
+
+import Foundation
+//class show_ad_Controller: UIViewController, GADInterstitialDelegate {
